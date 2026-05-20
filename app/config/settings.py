cd92745982da4e48d511b3896f7c84116ee056e8
Sena <rodrigovlzsena@gmail.com>
2026-05-20 18:56:39 -0500

    variables de entorno se cargan y leen

---
+++ app/config/settings.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app_name = os.environ.get('APP_NAME')
+app_version = os.environ.get('APP_VERSION')
+admin_user = os.environ.get('ADMIN_USER')
+
+print(app_name)
+print(app_version)
+print(admin_user)
